3d16b2ead0f2beed6f8db3f87cf13a00b8398b7f
Roberto Alba <roberto@cbisystems.mx>
2021-02-01 17:34:15 -0600

    Initial Commit

---
+++ SIRHCOBNOMINA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SIRHCOBNOMINA
+//
+//  Created by Desarrollador iOS on 01/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
